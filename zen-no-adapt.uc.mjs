ed25542a1fcfca4514c25671399affc6fd724c31
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2026-03-01 21:33:21 -0500

    refactor: Dark mode detection
    Simplify dark mode detection by using the built-in Zen variable.

--- zen-no-adapt.uc.mjs
+++ zen-no-adapt.uc.mjs
@@ -23,17 +23,7 @@ await waitForDependencies();
 
 // Replace default dark mode detection with no adaptation method
 const oldDarkDetect = gZenThemePicker.shouldBeDarkMode;
-gZenThemePicker.shouldBeDarkMode = () => {
-  const colorScheme = Services.prefs.getIntPref("zen.view.window.scheme");
-  if (colorScheme === 0) {
-    return true;
-  } else if (colorScheme === 1) {
-    return false;
-  } else {
-    // Automatically detect color scheme
-    oldDarkDetect();
-  }
-};
+gZenThemePicker.shouldBeDarkMode = () => gZenThemePicker.isDarkMode;
 
 // Refresh color scheme using new behavior
 gZenThemePicker.onWorkspaceChange(gZenWorkspaces.getActiveWorkspace());
